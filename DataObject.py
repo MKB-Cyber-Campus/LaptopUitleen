c34f56494e83638bab1a1fcf5eb5f32294e8d059
M4rt1nV <martin_visser93@hotmail.com>
2025-03-13 13:20:32 +0100

    Added in the Data Object and its init

---
+++ DataObject.py
@@ -0,0 +1,5 @@
+class DataObject:
+    def __init__(self, leerlingnummer, laptopnummer, uitgiftedatum):
+        self.leerlingnummer = leerlingnummer
+        self.laptopnummer = laptopnummer
+        self.uitgiftedatum = uitgiftedatum
