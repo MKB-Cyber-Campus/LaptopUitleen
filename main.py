8353281216bd2f0a7b9e1ab4d4e623c85dae18ce
M4rt1nV <martin_visser93@hotmail.com>
2025-03-14 11:07:58 +0100

    Added in function to properly start and show the UI

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from UI import Ui
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QPushButton, QMessageBox
 import sys
 
-if __name__ == "main":
+if __name__ == "__main__":
     app = QApplication(sys.argv)  # Create an instance of QtWidgets.QApplication
     window = Ui()  # Create an instance of our class
     app.exec_()  # Start the application
