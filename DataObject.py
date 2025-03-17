02c60079e56e9c282af544207bad6094668bf527
M4rt1nV <martin_visser93@hotmail.com>
2025-03-17 11:49:06 +0100

    Removed data object due to redundancy

--- DataObject.py
+++
@@ -1,5 +0,0 @@
-class DataObject:
-    def __init__(self, leerlingnummer, laptopnummer, uitgiftedatum):
-        self.leerlingnummer = leerlingnummer
-        self.laptopnummer = laptopnummer
-        self.uitgiftedatum = uitgiftedatum
