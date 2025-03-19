5e0edaffe491432d04ca905208d4f8e882a9ac11
M4rt1nV <martin_visser93@hotmail.com>
2025-03-19 15:03:30 +0100

    Updated functionality for the UI class, letting it work with Datahandler

--- UI.py
+++ UI.py
@@ -5,6 +5,9 @@ from PyQt5.QtCore import pyqtSignal, QThread, QObject
 from PyQt5.uic.uiparser import QtWidgets
 import sys
 from DataHandler import DataHandler
+from datetime import datetime
+import openpyxl
+from openpyxl.styles import PatternFill, Font
 
 class Ui(QMainWindow):
     def __init__(self):
@@ -32,10 +35,27 @@ class Ui(QMainWindow):
         messagewarnings = {"EmptyPersoonBarcode" : "Het veld Leerling/Leraar barcode is leeg!",
                            "EmptyLaptopBarcode" : "Het veld Laptop Barcode is leeg!",
                            "EmptySearchBarcode" : "Het veld Zoek op Barcode is leeg!",
-                           "BothBarcodeEmpty" : "De velden Leerling/Leraar barcode en Laptop Barcode zijn leeg!"}
+                           "BothBarcodeEmpty" : "De velden Leerling/Leraar barcode en Laptop Barcode zijn leeg!",
+                           "NoResult" : "Er is geen resultaat met de barcode gevonden",
+                           "SaveError" : "Het bestand opslaan is niet gelukt"}
         msg.setText(messagewarnings[type])
         msg.setIcon(QMessageBox.Warning)
         msg.exec_()
+
+
+    def ResultMSG(self, Resultaat):
+        '''
+        Geeft een window weer met het zoekresultaat
+
+        Args:
+            Resultaat: Het weer te geven resultaat
+        '''
+        msg = QMessageBox()
+        msg.setWindowTitle("Resultaat")
+        msg.setText(Resultaat)
+        msg.setIcon(QMessageBox.Information)
+        msg.exec_()
+
 
     def RegistreerUitleenButtonClicked(self):
         '''
@@ -44,6 +64,7 @@ class Ui(QMainWindow):
         '''
         PersoonBarcode = self.GetPersonBarcode()
         LaptopBarcode = self.GetLaptopBarcode()
+        Datum = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
         if PersoonBarcode == "" and LaptopBarcode == "":
             self.WarnMSG("BothBarcodeEmpty")
@@ -54,7 +75,8 @@ class Ui(QMainWindow):
         elif LaptopBarcode == "":
             self.WarnMSG("EmptyLaptopBarcode")
             return
-        
+        self.datahandler.DataWrite(PersoonBarcode, LaptopBarcode, Datum)
+        self.ClearUitleenBoxes()
         
 
     def ZoekUitleenButtonClicked(self):
@@ -66,13 +88,24 @@ class Ui(QMainWindow):
             self.WarnMSG("EmptySearchBarcode")
             return
         
+        Resultaat = self.datahandler.DataSearch(ZoekBarcode)
+        if Resultaat == []:
+            self.WarnMSG("NoResult")
+            return
+        
+        ResultaatString = f"ID: {Resultaat[0]}, Leerling/Leraar: {Resultaat[1]}, Laptop: {Resultaat[2]}, Uitgifte Datum: {Resultaat[3]}"
+        self.ResultMSG(ResultaatString)
+
+        self.ClearZoekBoxes()
+        
 
     def ExporteerNaarExcelButtonClicked(self):
         '''
         Exporteert de gegevens naar een excel bestand wat makkelijk leesbaar is
         '''
-        # Kijken of dit met Pandas goed wil, anders oude unit gebruiken
-        pass
+        Registraties = self.datahandler.DataDump()
+        self.MaakExcel(Registraties)
+        
 
     def ToggleFullscreenButtonClicked(self):
         '''
@@ -85,6 +118,7 @@ class Ui(QMainWindow):
             self.showFullScreen()
             self.ToggleFullscreenButton.setText("Toggle Windowed")
 
+
     def GetPersonBarcode(self):
         '''
         Functie om Leerling/Leraar barcode uit de textbox te halen
@@ -93,6 +127,7 @@ class Ui(QMainWindow):
             De text uit PersoonBarcodeLineEdit
         '''
         return self.PersoonBarcodeLineEdit.text()
+
 
     def GetLaptopBarcode(self):
         '''
@@ -103,6 +138,7 @@ class Ui(QMainWindow):
         '''
         return self.LaptopBarcodeLineEdit.text()
 
+
     def GetSearchBarcode(self):
         '''
         Functie om te zoeken barcode uit de textbox te halen
@@ -111,6 +147,67 @@ class Ui(QMainWindow):
             De text uit ZoekLaptopBarcodeLineEdit
         '''
         return self.ZoekLaptopBarcodeLineEdit.text()
+
+
+    def ClearUitleenBoxes(self):
+        '''
+        Functie om invoerboxes leeg te maken nadat invoer is gebruikt of opgeslagen
+        '''
+        self.PersoonBarcodeLineEdit.setText("")
+        self.LaptopBarcodeLineEdit.setText("")
+
+
+    def ClearZoekBoxes(self):
+        '''
+        Functie om invoerbox leeg te maken nadat informatie is opgezocht
+        '''
+        self.ZoekLaptopBarcodeLineEdit.setText("")
+
+
+    def MaakExcel(self, Registraties):
+        '''
+        Maakt het Excel bestand van de uitgeleende laptops, en vult deze met de data uit de database
+
+        Args:
+            Registraties: Alle informatie uit de database
+        '''
+        DatumVandaag = datetime.now().strftime('%Y-%m-%d')
+        Bestandsnaam = f"Uitleen_{DatumVandaag}.xlsx"
+
+        workbook = openpyxl.Workbook()
+        sheet = workbook.active
+        sheet.title = "Uitleen"
+
+        header_fill = PatternFill(start_color="00C0C0C0", end_color="00C0C0C0", fill_type="solid")
+        header_font = Font(bold=True, color="FFFFFF")
+
+        headers = ["ID", "Leerling/Leraar Barcode", "Laptop Barcode",   "Uitgifte Datum", "Inleverdatum"]
+        sheet.append(headers)
+        for col in range(1, len(headers) + 1):
+            sheet.cell(row=1, column=col).fill = header_fill
+            sheet.cell(row=1, column=col).font = header_font
+
+        for index, registratie in enumerate(Registraties, start=2):
+            sheet.append(registratie[:-1])
+
+        if registratie[5] == 1:
+            sheet.row_dimensions[index].fill = PatternFill(start_color="00FF0000", end_color="00FF0000", fill_type="solid")
+        elif registratie[4]:
+            sheet.row_dimensions[index].fill = PatternFill(start_color="00CCFFCC", end_color="00CCFFCC", fill_type="solid")
+
+        sheet.column_dimensions['B'].width = 30
+        sheet.column_dimensions['C'].width = 20
+        sheet.column_dimensions['D'].width = 20
+        sheet.column_dimensions['E'].width = 20
+    	
+        try:
+            workbook.save(Bestandsnaam)
+            self.ResultMSG(f"Gegevens succesvol geëxporteerd naar {Bestandsnaam}")
+        except Exception as e:
+            self.WarnMSG("SaveError")
+            
+
+        
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)  # Create an instance of QtWidgets.QApplication
