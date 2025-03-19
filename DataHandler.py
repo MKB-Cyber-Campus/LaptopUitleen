8a22b84bd65da795d9ddd995b31419667a5ff04d
M4rt1nV <martin_visser93@hotmail.com>
2025-03-19 15:03:10 +0100

    Updated the functions for the DataHandler class

--- DataHandler.py
+++ DataHandler.py
@@ -5,11 +5,11 @@ class DataHandler():
         '''
         The init establishes the connection with the database, and creates 
         '''
-        conn = sqlite3.connect('laptop_uitleningen.db')
-        c = conn.cursor()
-        self.CreateTable(c, conn)
+        self.conn = sqlite3.connect('laptop_uitleningen.db')
+        self.c = self.conn.cursor()
+        self.CreateTable()
 
-    def CreateTable(self, c, conn):
+    def CreateTable(self):
         '''
         Creates a table with specific parameters within the database
         
@@ -18,7 +18,7 @@ class DataHandler():
             conn : The connection to the database
         
         '''
-        c.execute('''CREATE TABLE IF NOT EXISTS uitleenregistraties (
+        self.c.execute('''CREATE TABLE IF NOT EXISTS uitleenregistraties (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 leerling_medewerker_barcode TEXT,
                 laptop_barcode TEXT,
@@ -26,7 +26,7 @@ class DataHandler():
                 inleverdatum TEXT,
                 verwijderd INTEGER DEFAULT 0
             )''')
-        conn.commit()
+        self.conn.commit()
 
     def DataWrite(self, PersoonBarcode, LaptopBarcode, Datum):
         '''
@@ -36,16 +36,26 @@ class DataHandler():
             LaptopBarcode: Barcode nummer van een laptop die door een persoon wordt geleend
             Datum: Datum waarop de laptop wordt geleend
         '''
-        pass
+        self.c.execute("INSERT INTO uitleenregistraties (leerling_medewerker_barcode, laptop_barcode, uitgifte_datum) VALUES (?, ?, ?)", 
+              (PersoonBarcode, LaptopBarcode, Datum))
+        self.conn.commit()
 
     def DataSearch(self, ZoekBarcode):
         '''
         Zoekt op een Barcode, om de omringende data op te halen die er bij hoort
         '''
-        pass
+        self.c.execute("SELECT * FROM uitleenregistraties WHERE (leerling_medewerker_barcode = ? OR laptop_barcode = ?) AND inleverdatum IS NULL ORDER BY uitgifte_datum DESC", 
+              (ZoekBarcode, ZoekBarcode))
+        resultaten = self.c.fetchall()
+        return resultaten[0]
 
     def DataDump(self):
         '''
         Leest de data uit de database file, en schrijft deze weg naar een excel bestand
+        
+        Return:
+            De data uit het .db bestand
         '''
-        pass
+        self.c.execute("SELECT * FROM uitleenregistraties")
+        registraties = self.c.fetchall()
+        return registraties
